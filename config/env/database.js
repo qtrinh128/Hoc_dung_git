340134b4340afa25306d5b55ca5af69b78036d66
qtrinh128 <qtrinh128@gmail.com>
2018-08-28 12:20:54 +0700

    fffafafa

--- config/env/database.js
+++ config/env/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
-      host: 'mysql',
+      host: 'localhost',
       port: '3306',
       user: 'root',
-      password: '12313123',
+      password: '123123',
       database: 'fitisoft'
     }
   };
